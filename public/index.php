6dce4b1e461f84d39fcc58d24ac7eef53c627fc7
huohongjian <1123576798@qq.com>
2017-09-03 23:00:06 +0800

    mm

--- public/index.php
+++ public/index.php
@@ -2,26 +2,19 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-//echo $_SERVER['HTTP_HOST'].""; #localhost
-//echo $_SERVER['PHP_SELF'].""; #/blog/testurl.php
-
-
+/*
 define('SITE_NAME','China FreeBSD');
 $the_file_path = $_SERVER['PHP_SELF'];
 $findme   = '/index.php';
 $pos = strpos($the_file_path, $findme);
 $target_path = substr($the_file_path, 0,$pos);
 $site_url = "http://".$_SERVER['HTTP_HOST'].$target_path;
-
 define('SITE_URL',$site_url);
-
-//echo SITE_URL;
-
+*/
 
 
 
 require_once('../vendor/autoload.php');
-
 
 $app = new \Slim\App();
 
@@ -34,18 +27,15 @@ $container['view'] = function($c) {
     $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
     $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
 
-$request = $c->request;
-    $uri = $request->getUri();
-    $scheme= $uri->getScheme();
-    $host = $uri->getHost();
-    $port = $uri->getPort() ;
-    $baseUrl = $scheme."://".$host.":".$port;
-
-
-    
-    $view->getEnvironment()->addGlobal('baseURL', SITE_URL);
-
-echo SITE_URL;
+	$request = $c->request;
+	$uri = $request->getUri();
+	$scheme= $uri->getScheme();
+	$host = $uri->getHost();
+	$port = $uri->getPort() ;
+	if($port) $port = ':'.$port;
+	$path = $uri->getBasePath();
+	$baseUrl = $scheme."://".$host.$port.$path;
+	$view->getEnvironment()->addGlobal('baseURL', $baseUrl);
 
     return $view;
 };
